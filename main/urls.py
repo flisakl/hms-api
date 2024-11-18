568f393415616444e7046db01ab46bf021ed5f75
Łukasz Flisak <lu.dev00@gmail.com>
2024-11-18 15:06:00 +0100

    Add ninja API to urls

--- main/urls.py
+++ main/urls.py
@@ -19,6 +19,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .api import api
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

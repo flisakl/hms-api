b4bc9422c4b46c06714301d3f4415d4871fd6a6f
Łukasz Flisak <lu.dev00@gmail.com>
2024-11-21 22:22:58 +0100

    Add Artist model

--- artists/models.py
+++ artists/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='artists', null=True, blank=True)
+
+    def __str__(self):
+        return self.name

235aa7b2d4277a80e9bed65631c3f5b2bf31a0fe
Łukasz Flisak <lu.dev00@gmail.com>
2024-11-21 17:34:43 +0100

    Add template for artists app

--- main/api.py
+++ main/api.py
@@ -1,6 +1,8 @@
 from ninja import NinjaAPI
 
 from users.api import router as auth_router
+from artists.api import router as artists_router
 
 api = NinjaAPI()
 api.add_router('/users/', auth_router)
+api.add_router('/artists/', artists_router)

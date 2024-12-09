8a33ddee1ef578d4dbf25eb8bdf15b5956cf5fb7
Łukasz Flisak <lu.dev00@gmail.com>
2024-12-09 10:23:40 +0100

    Add template for albums app

--- main/api.py
+++ main/api.py
@@ -2,7 +2,9 @@ from ninja import NinjaAPI
 
 from users.api import router as auth_router
 from artists.api import router as artists_router
+from albums.api import router as albums_router
 
 api = NinjaAPI()
 api.add_router('/users/', auth_router)
 api.add_router('/artists/', artists_router)
+api.add_router('/albums/', albums_router)
--- main/settings.py
+++ main/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users',
     'artists',
+    'albums',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

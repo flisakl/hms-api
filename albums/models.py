9d058e64645330b7c983880b8b6663163589bcc0
Łukasz Flisak <lu.dev00@gmail.com>
2024-12-09 10:31:05 +0100

    Add Album model

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from artists.models import Artist
 
-# Create your models here.
+
+year_validators = [MinValueValidator(limit_value=0)]
+
+class Album(models.Model):
+    name = models.CharField(max_length=200)
+    cover = models.ImageField(upload_to='albums', null=True, blank=True)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    genre = models.CharField(max_length=50, null=True, blank=True)
+    year = models.IntegerField(null=True, blank=True, validators=year_validators)
+
+    class Meta:
+        constraints = [
+            models.constraints.UniqueConstraint(
+                fields=['artist_id', 'name'], name='unique_artist_album'
+            )
+        ]
